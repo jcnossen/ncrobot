be8ac06b68da7c688f15c6cba0ae9fd6238f0265
Johan.de.Ruiter <Johan.de.Ruiter@539a5a7e-e5a0-4617-9da5-5f7f46e2b2b9>
2009-12-11 15:24:12 +0000

    met optimizertest.cpp
    git-svn-id: http://subversion.assembla.com/svn/ncrobot@24 539a5a7e-e5a0-4617-9da5-5f7f46e2b2b9

---
+++ code/OptimizerTest.cpp
@@ -0,0 +1,94 @@
+#include "StdIncl.h"
+#include "Optimizer.h"
+#include "GAOptimizer.h"
+#include "swarm/swarm.h"
+
+
+int DIM=30;
+double pi=3.1415926535897;
+  
+float calcAckley(float *params) {
+  float f=20+exp(1);
+
+  float sumsq=0;
+  for(int k=0;k<DIM;k++)
+    sumsq+=params[k]*params[k];
+  f+=-20*exp(-0.2*sqrt(sumsq/DIM));
+
+  float sumcos=0;
+  for(int k=0;k<DIM;k++)
+    sumcos+=cos(2*pi*params[k]);
+  f-=exp(sumcos/DIM);
+
+  //f = 20 - 20 * exp(-0.2 * sqrt(sum(x .^ 2, 2) / N)) - exp(sum(cos(2 * pi * x), 2) / N) + exp(1);
+
+  return f;
+}
+
+
+int main(int argc, char*argv[])
+{
+  int i,j,k;
+  
+  Optimizer* optimizer = 0;
+  
+  
+  std::vector<ParameterRange> ranges(DIM);
+  for(i=0;i<DIM;i++)
+  {
+    ranges[i].min=-50;
+    ranges[i].max=50;
+  }
+
+  int popSize = 100;
+  
+  if (true) {
+    SwarmConfig cfg;
+    cfg.popSize = popSize;
+    cfg.phi1=cfg.phi2=1.49618;
+    cfg.omega=0.7298;
+
+    Swarm *swarm = new Swarm(cfg, ranges);
+    optimizer = swarm;
+  }
+  else {
+    GAConfig cfg;
+
+    cfg.size = popSize;
+    cfg.pointMutationRate = 1.0f/DIM;
+    cfg.crossoverMutationRate = 0.01f;
+    GAOptimizer *ga= new GAOptimizer(cfg, ranges);
+    optimizer = ga;
+  }
+    
+
+  float BEST=-1e15;
+
+  for(i=0;i<1000;i++) {
+    optimizer->update();
+    
+    for (j=0;j<optimizer->getSize();j++) {
+      float* params = optimizer->getStateVector(j);    
+      
+      float fitness = -calcAckley(params);
+
+      if(fitness>BEST)
+      {
+        BEST=fitness;
+        printf("Best=%.5f (iter %d)\n",BEST,i);
+        for(k=0;k<DIM;k++) printf("(%.3lf)",params[k]);
+        printf("\n\n");
+      }
+      
+      optimizer->setFitness(j, fitness);
+    }
+  }  
+
+  delete optimizer;
+
+  return 0;
+}
+
+
+
+
